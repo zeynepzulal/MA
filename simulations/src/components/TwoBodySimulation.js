3caa8dccc103c99f30425345cedc765719128398
Zeynep Zülal <119702784+zeynepzulal@users.noreply.github.com>
2024-10-17 22:42:44 +0200

    2 body simulation

--- simulations/src/components/TwoBodySimulation.js
+++ simulations/src/components/TwoBodySimulation.js
@@ -1,16 +1,88 @@
-import React, {useEffect } from 'react'
+import React, { useEffect, useRef} from 'react'
 
 const TwoBodySimulation = () => {
-   
-    useEffect(() => {
-        let body1 = {x: 300,y: 300,vx: 0,vy: 2,mass: 1000,};
 
-        let body2 = {x: 500,y: 300,vx: 0,vy: -2,mass: 1000,}
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        canvas.width = 800;
+        canvas.height = 600;
+
+        let body1 = { x: 300, y: 300, vx: 0, vy: 2, mass: 1000, };
+
+        let body2 = { x: 500, y: 300, vx: 0, vy: -2, mass: 1000, };
+
+        const G = 0.05;
+        const damping = 0.999;
+
+        function update() {
+
+            context.clearRect(0,0,canvas.width, canvas.height);
+            //distance
+            const dx = body2.x - body1.x;
+            const dy = body2.y - body1.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            //gravitational force
+            const force = (G * body1.mass * body2.mass) / (distance * distance);
+
+            //direction of force
+            const ax1 = (force * dx) / (body1.mass * distance);
+            const ay1 = (force * dy) / (body1.mass * distance);
+            const ax2 = -(force * dx) / (body2.mass * distance);
+            const ay2 = -(force * dy) / (body2.mass * distance);
+
+            //update v 
+            body1.vx += ax1;
+            body1.vy += ay1;
+            body2.vx += ax2;
+            body2.vy += ay2;
+
+            //damping
+            body1.vx *= damping;
+            body1.vy *= damping;
+            body2.vx *= damping;
+            body2.vy *= damping;
+
+            //update position => new position = current position + v.t
+            body1.x += body1.vx; 
+            body1.y += body1.vy;
+            body2.x += body2.vx;
+            body2.y += body2.vy;
+
+            // boundries
+            if(body1.x <= 0 || body1.x >= canvas.width) body1.vx = -body1.vx;
+            if(body1.y <= 0 || body1.y >= canvas.height) body1.vy = -body1.vy;
+            if(body2.x <= 0 || body2.x >= canvas.width) body2.vx = -body2.vx;
+            if(body2.y <= 0 || body2.y >= canvas.height) body2.vy = -body2.vy;
+
+            drawBody(body1, 'blue');
+            drawBody(body2, 'red');
+        }
+
+        function drawBody(body ,color){
+            context.beginPath();
+            context.arc(body.x,body.y,20,0,2* Math.PI);
+            context.fillStyle = color;
+            context.fill();
+            context.stroke();
+        }
+
+        function animate(){
+            update();
+            requestAnimationFrame(animate);
+        }
+
+        animate();
     }, []);
 
 
 
-    return (<div></div>)
-}
+    return (<canvas ref={canvasRef} style={{border: '1px solid black'}}/>)
+
+};
 
 export default TwoBodySimulation
