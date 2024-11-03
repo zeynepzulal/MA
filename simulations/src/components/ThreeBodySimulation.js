3211e687c6e53706f4e3ca3568da6b0403d29c1d
Zeynep Zülal <119702784+zeynepzulal@users.noreply.github.com>
2024-11-03 19:50:32 +0100

    periodic solutions

--- simulations/src/components/ThreeBodySimulation.js
+++ simulations/src/components/ThreeBodySimulation.js
@@ -1,51 +1,49 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 
 const ThreeBodySimulation = () => {
 
     const canvasRef = useRef(null);
+    const [initialCondition, setInitialCondition] = useState('figureEight')
+
+    const InitialConditions = {
+        // inital values for figure-8 from  https://www.youtube.com/watch?v=UC40kDpAI8M&list=LL&index=1&t=1032s
+        figureEight: [
+            { x: 400 + 0.97000436 * 200, y: 300 - 0.24308753 * 200, vx: 0.93240737 / 2, vy: 0.86473146 / 2, mass: 1000 },
+            { x: 400 - 0.97000436 * 200, y: 300 + 0.24308753 * 200, vx: 0.93240737 / 2, vy: 0.86473146 / 2, mass: 1000 },
+            { x: 400, y: 300, vx: -0.93240737, vy: -0.86473146, mass: 1000 }
+        ]
+    };
+
+    const [bodies, setBodies] = useState(InitialConditions.figureEight);
+
+    let path1 = [];
+    let path2 = [];
+    let path3 = [];
+
+    const handleSelectChange = (event) => {
+        const selectedCase = event.target.value;
+        setInitialCondition(selectedCase);
+        setBodies(InitialConditions[selectedCase]);
+    };
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
         canvas.width = 800;
         canvas.height = 600;
-/*
-        let body1 = { x: 300, y: 300, vx: 0, vy: 2, mass: 1000 , path:[]};
-        let body2 = { x: 500, y: 300, vx: 0, vy: -2, mass: 1000 , path: []};
-        let body3 = { x: 400, y: 100, vx: 1.5, vy: 0, mass: 1000, path: []};
-*/
-// inital values for figure-8 from  https://www.youtube.com/watch?v=UC40kDpAI8M&list=LL&index=1&t=1032s
-        let body1 = { 
-            x: 400 + 0.97000436 * 200,   
-            y: 300 - 0.24308753 * 200, 
-            vx: 0.93240737 / 2, 
-            vy: 0.86473 / 2, 
-            mass: 1000,
-            path:[]
-        };
-        let body2 = { 
-            x: 400 - 0.97000436 * 200, 
-            y: 300 + 0.24308753 * 200, 
-            vx: 0.93240737 / 2, 
-            vy: 0.86473 / 2, 
-            mass: 1000,
-            path:[]
-        };
-        let body3 = { 
-            x: 400, 
-            y: 300, 
-            vx: -0.93240737, 
-            vy: -0.86473146, 
-            mass: 1000,
-            path:[]
-        };
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        path1 = [];
+        path2 = [];
+        path3 = [];
+
         const G = 0.1;
-        const maxTrailLength = 200;
-        
+        const softening = 0.1;
+
         function calculateForces(bodyA, bodyB) {
             const dx = bodyB.x - bodyA.x;
             const dy = bodyB.y - bodyA.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
+            const distance = Math.sqrt(dx * dx + dy * dy + softening);
             const force = (G * bodyA.mass * bodyB.mass) / (distance * distance);
 
             const ax = (force * dx) / (bodyA.mass * distance);
@@ -55,78 +53,98 @@ const ThreeBodySimulation = () => {
         }
 
         function update() {
-            context.clearRect(0, 0, canvas.width, canvas.height);
-
             //force on body1
-            const force12 = calculateForces(body1, body2);
-            const force13 = calculateForces(body1, body3);
+            const force12 = calculateForces(bodies[0], bodies[1]);
+            const force13 = calculateForces(bodies[0], bodies[2]);
 
             //force on body2
-            const force21 = calculateForces(body2, body1);
-            const force23 = calculateForces(body2, body3);
+            const force21 = calculateForces(bodies[1], bodies[0]);
+            const force23 = calculateForces(bodies[1], bodies[2]);
 
             //force on body3
-            const force31 = calculateForces(body3, body1);
-            const force32 = calculateForces(body3, body2);
+            const force31 = calculateForces(bodies[2], bodies[0]);
+            const force32 = calculateForces(bodies[2], bodies[1]);
 
             // update v
-            body1.vx += force12.ax + force13.ax;
-            body1.vy += force12.ay + force13.ay;
+            bodies[0].vx += force12.ax + force13.ax;
+            bodies[0].vy += force12.ay + force13.ay;
 
-            body2.vx += force21.ax + force23.ax;
-            body2.vy += force21.ay + force23.ay;
+            bodies[1].vx += force21.ax + force23.ax;
+            bodies[1].vy += force21.ay + force23.ay;
 
-            body3.vx += force31.ax + force32.ax;
-            body3.vy += force31.ay + force32.ay;
+            bodies[2].vx += force31.ax + force32.ax;
+            bodies[2].vy += force31.ay + force32.ay;
 
             // update position Dt  = 1s
-            body1.x += body1.vx;
-            body1.y += body1.vy;
-            body2.x += body2.vx;
-            body2.y += body2.vy;
-            body3.x += body3.vx;
-            body3.y += body3.vy;
+            bodies[0].x += bodies[0].vx;
+            bodies[0].y += bodies[0].vy;
+            bodies[1].x += bodies[1].vx;
+            bodies[1].y += bodies[1].vy;
+            bodies[2].x += bodies[2].vx;
+            bodies[2].y += bodies[2].vy;
 
-            updateTrailAndDraw(body1, 'blue');
-            updateTrailAndDraw(body2, 'red');
-            updateTrailAndDraw(body3, 'green');
-            drawBody(body1, 'blue');
-            drawBody(body2, 'red');
-            drawBody(body3, 'green');
+            path1.push({ x: bodies[0].x, y: bodies[0].y });
+            path2.push({ x: bodies[1].x, y: bodies[1].y });
+            path3.push({ x: bodies[2].x, y: bodies[2].y });
+
+            if (path1.length > 400) path1.shift();
+            if (path2.length > 400) path2.shift();
+            if (path3.length > 400) path3.shift();
         }
 
-        function updateTrailAndDraw(body, color) {
-            body.path.push({ x: body.x, y: body.y });
-            if (body.path.length > maxTrailLength) body.path.shift();
 
-            context.strokeStyle = color;
-            context.beginPath();
-            for (let i = 0; i < body.path.length - 1; i++) {
-                context.moveTo(body.path[i].x, body.path[i].y);
-                context.lineTo(body.path[i + 1].x, body.path[i + 1].y);
-            }
-            context.stroke();
-        }
 
         function drawBody(body, color) {
             context.beginPath();
-            context.arc(body.x, body.y, 20, 0, 2 * Math.PI);
+            context.arc(body.x, body.y, 3, 0, 2 * Math.PI);
             context.fillStyle = color;
+            context.shadowColor = color;
+            context.shadowBlur = 10;
             context.fill();
+        }
+
+        function drawPath(path, color) {
+            context.beginPath();
+            context.moveTo(path[0].x, path[0].y);
+            for (let i = 1; i < path.length; i++) {
+                context.lineTo(path[i].x, path[i].y);
+            }
+            context.strokeStyle = color;
+            context.lineWidth = 1.5;
             context.stroke();
         }
+
         function animate() {
+            context.fillStyle = 'rgba(0,0,0,1)';
+            context.fillRect(0, 0, canvas.width, canvas.height);
+
             update();
+
+            drawPath(path1, 'rgb(66, 135, 245)');
+            drawPath(path2, 'rgb(245, 66, 66)');
+            drawPath(path3, 'rgb(240, 240, 66)');
+
+            drawBody(bodies[0], 'rgb(66, 135, 245)');
+            drawBody(bodies[1], 'rgb(245, 66, 66)');
+            drawBody(bodies[2], 'rgb(240, 240, 66)');
+
             requestAnimationFrame(animate);
         }
 
         animate();
 
-    }, []);
+    }, [bodies]);
 
     return (
-        <canvas ref={canvasRef} style={{ border: '1px solid black' }} />
-    )
-}
+        <div style={{ textAlign: 'center', padding: '10px' }}>
+            <select value={initialCondition} onChange={handleSelectChange} style={{ marginBottom: '10px', padding: '5px' }}>
+                <option value="figureEight">Figure Eight</option>
+            </select>
+            <canvas ref={canvasRef} style={{ border: '1px solid black', display: 'block', margin: '0 auto', background: 'black' }} />
+        </div>
+
+    );
+};
+
 
 export default ThreeBodySimulation
